a247dcae53db37eda58a823af2302eae1338ac7d
garozaev <g.a.rozaev@gmail.com>
2023-12-03 14:14:37 +0300

    1hour

---
+++ assets/scripts/questions.js
@@ -0,0 +1,12 @@
+
+let str1 = 'What is the World?'
+  console.log(str1.indexOf('W'));
+
+  let str2 = `What 
+  is 
+  the 
+  World?`;
+  console.log(str2.lastIndexOf('What'));
+
+  let str3 = 'Wtat is the World?';
+  console.log(str3.search(/world/i));
